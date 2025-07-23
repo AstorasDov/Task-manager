9fa3792d6b193adcc97d79e29ac809b14e0dfd13
AstorasDov <astorasdov@gmail.com>
2025-07-23 16:53:27 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,14 @@
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const container = document.querySelector("#container");
 
+const sleep = (ms) => { return new Promise(resolve=>setTimeout(resolve,ms)); }
+
+addTaskBtn.addEventListener("mousedown",async (e)=>{
+    addTaskBtn.style.width = "60px";
+    await sleep(100);
+    addTaskBtn.style.width = "200px";
+});
+
 async function loadTasks () {
     const response = await fetch('/api/tasks');
     const json = await response.json();
@@ -9,7 +17,13 @@ async function loadTasks () {
     for(const task of tasks){
         const taskHTML = `<label class="task">${task.name}</label>`;
         container.innerHTML += taskHTML;
-    }
+    };const taskEl = Array.from(container.querySelectorAll('.task'));
+    taskEl.forEach(task=>{
+        task.addEventListener("click",async ()=>{
+            await sleep(150);
+            task.remove();
+        })
+    })
 }
 
 window.addEventListener("DOMContentLoaded",loadTasks);
